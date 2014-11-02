bbb48e4af7bf2eb0f4ea69d1034297377a40fc20
Keith D. Moore <keith@kdmooreconsulting.com>
2014-11-02 12:05:19 -0600

    Fix issue associated with other pages unable to scroll
    After loading a page with an element having the no-scroll directive, other pages were not able to scroll when navigating to them.

--- demo/app.js
+++ demo/app.js
@@ -12,13 +12,13 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
 
 })
 
-.directive('noScroll', function($document) {
+.directive('noScroll', function() {
 
   return {
     restrict: 'A',
     link: function($scope, $element, $attr) {
 
-      $document.on('touchmove', function(e) {
+      $element.on('touchmove', function(e) {
         e.preventDefault();
       });
     }
